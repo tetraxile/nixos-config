071af6d1b10e82e3b8fa7302d36c0cb00ca54946
tetraxile <tetraxile@proton.me>
2025-09-29 19:27:24 +0100

    nushell: add carapace and starship

--- home/nushell/default.nix
+++ home/nushell/default.nix
@@ -2,11 +2,23 @@
 {
   imports = [ ./scripts ];
 
-  programs.nushell = {
-    enable = true;
-    configFile.source = ./config.nu;
-    shellAliases = {
-      ghidra = "_JAVA_AWT_WM_NONREPARENTING=1 ${pkgs.ghidra}/lib/ghidra/ghidraRun";
+  programs = {
+    nushell = {
+      enable = true;
+      configFile.source = ./config.nu;
+      shellAliases = {
+        ghidra = "_JAVA_AWT_WM_NONREPARENTING=1 ${pkgs.ghidra}/lib/ghidra/ghidraRun";
+      };
+    };
+
+    carapace = {
+      enable = true;
+      enableNushellIntegration = true;
+    };
+
+    starship = {
+      enable = true;
+      enableNushellIntegration = true;
     };
   };
 }
