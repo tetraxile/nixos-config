a359e146cca42d271ce0400008788912dfc4d710
tetraxile <tetraxile@proton.me>
2026-04-25 21:12:16 +0200

    use caps lock as fcitx layout switcher

--- home/i3/default.nix
+++ home/i3/default.nix
@@ -21,12 +21,13 @@
     enable = true;
     config =
       let
-        mod = "Mod4";
+        meta = "Mod4";
         alt = "Mod1";
+        mod = "Mod3";
         modeSystem = "loc[k], [l]ogout, [s]uspend, [r]eboot, [S]hutdown";
       in
       {
-        modifier = mod;
+        modifier = meta;
 
         fonts = {
           names = [ "JetBrainsMono NF" ];
@@ -37,103 +38,104 @@
           # lib.mkForce
           {
             # start terminal
-            "${mod}+Return" = "exec ${pkgs.wezterm}/bin/wezterm";
+            "${meta}+Return" = "exec ${pkgs.wezterm}/bin/wezterm";
 
             # start clipmenu
-            "${mod}+z" = "exec --no-startup-id ${pkgs.clipmenu}/bin/clipmenu";
+            "${meta}+z" = "exec --no-startup-id ${pkgs.clipmenu}/bin/clipmenu";
 
             # start passmenu
-            "${mod}+p" = "exec --no-startup-id ${pkgs.pass}/bin/passmenu";
+            "${meta}+p" = "exec --no-startup-id ${pkgs.pass}/bin/passmenu";
 
             # start dmenu
-            "${mod}+d" = "exec --no-startup-id ${./dmenu_run.nu}";
+            "${meta}+d" = "exec --no-startup-id ${./dmenu_run.nu}";
 
             # start browser
-            "${mod}+q" = "exec ${pkgs.floorp-bin}/bin/floorp";
+            "${meta}+q" = "exec ${pkgs.floorp-bin}/bin/floorp";
 
             # eyedropper
             # zoomer
 
             # switch input method
-            "${mod}+Tab" = "exec --no-startup-id fcitx5-remote -t";
+            "${mod}+1" = "exec fcitx5-remote -s keyboard-tetra";
+            "${mod}+2" = "exec fcitx5-remote -s mozc";
 
             # take screenshot and copy to clipboard
-            "${mod}+c" = "exec --no-startup-id escrotum -sC";
+            "${meta}+c" = "exec --no-startup-id escrotum -sC";
 
             # take screenshot and save to file
-            # "${mod}+Alt+c" = "exec escrotum -s '$HOME/Pictures/screenshot-%Y%m%d-%H%M%S.png'";
+            # "${meta}+Alt+c" = "exec escrotum -s '$HOME/Pictures/screenshot-%Y%m%d-%H%M%S.png'";
 
             # kill focused container
-            "${mod}+Shift+q" = "kill";
+            "${meta}+Shift+q" = "kill";
 
             # change focus
-            "${mod}+Up" = "focus up";
-            "${mod}+Down" = "focus down";
-            "${mod}+Left" = "focus left";
-            "${mod}+Right" = "focus right";
+            "${meta}+Up" = "focus up";
+            "${meta}+Down" = "focus down";
+            "${meta}+Left" = "focus left";
+            "${meta}+Right" = "focus right";
 
             # move focused container
-            "${mod}+Shift+Up" = "move up";
-            "${mod}+Shift+Down" = "move down";
-            "${mod}+Shift+Left" = "move left";
-            "${mod}+Shift+Right" = "move right";
+            "${meta}+Shift+Up" = "move up";
+            "${meta}+Shift+Down" = "move down";
+            "${meta}+Shift+Left" = "move left";
+            "${meta}+Shift+Right" = "move right";
 
             # split horizontally
-            "${mod}+h" = "split h";
+            "${meta}+h" = "split h";
 
             # split vertically
-            "${mod}+v" = "split v";
+            "${meta}+v" = "split v";
 
             # enter fullscreen for focused container
-            "${mod}+f" = "fullscreen toggle";
+            "${meta}+f" = "fullscreen toggle";
 
             # change container layout (stacked, tabbed, toggle split)
-            "${mod}+s" = "layout stacking";
-            "${mod}+w" = "layout tabbed";
-            "${mod}+e" = "layout toggle split";
+            "${meta}+s" = "layout stacking";
+            "${meta}+w" = "layout tabbed";
+            "${meta}+e" = "layout toggle split";
 
             # toggle tiling / floating
-            "${mod}+Shift+space" = "floating toggle";
+            "${meta}+Shift+space" = "floating toggle";
 
             # change focus between tiling / floating containers
-            "${mod}+space" = "focus mode_toggle";
+            "${meta}+space" = "focus mode_toggle";
 
             # focus the parent container
-            "${mod}+a" = "focus parent";
+            "${meta}+a" = "focus parent";
 
             # focus the child container
-            "${mod}+g" = "focus child";
+            "${meta}+g" = "focus child";
 
             # move current container to the next monitor
-            "${mod}+x" = "move workspace to output next";
+            "${meta}+x" = "move workspace to output next";
 
             # reload the configuration file
-            "${mod}+Shift+c" = "reload";
+            "${meta}+Shift+c" = "reload";
 
             # restart i3 inplace (preserves layout/session, can be used to upgrade it)
-            "${mod}+Shift+r" = "restart";
+            "${meta}+Shift+r" = "restart";
 
             # switch to workspace
-            "${mod}+1" = "workspace number 1";
-            "${mod}+2" = "workspace number 2";
-            "${mod}+3" = "workspace number 3";
-            "${mod}+4" = "workspace number 4";
-            "${mod}+5" = "workspace number 5";
-            "${mod}+6" = "workspace number 6";
-            "${mod}+7" = "workspace number 7";
-            "${mod}+8" = "workspace number 8";
-            "${mod}+9" = "workspace number 9";
+            "${meta}+1" = "workspace number 1";
+            "${meta}+2" = "workspace number 2";
+            "${meta}+3" = "workspace number 3";
+            "${meta}+4" = "workspace number 4";
+            "${meta}+5" = "workspace number 5";
+            "${meta}+6" = "workspace number 6";
+            "${meta}+7" = "workspace number 7";
+            "${meta}+8" = "workspace number 8";
+            "${meta}+9" = "workspace number 9";
 
             # move focused container to workspace
-            "${mod}+Shift+1" = "move container to workspace number 1";
-            "${mod}+Shift+2" = "move container to workspace number 2";
-            "${mod}+Shift+3" = "move container to workspace number 3";
-            "${mod}+Shift+4" = "move container to workspace number 4";
-            "${mod}+Shift+5" = "move container to workspace number 5";
-            "${mod}+Shift+6" = "move container to workspace number 6";
-            "${mod}+Shift+7" = "move container to workspace number 7";
-            "${mod}+Shift+8" = "move container to workspace number 8";
-            "${mod}+Shift+9" = "move container to workspace number 9";
+            "${meta}+Shift+1" = "move container to workspace number 1";
+            "${meta}+Shift+2" = "move container to workspace number 2";
+            "${meta}+Shift+3" = "move container to workspace number 3";
+            "${meta}+Shift+4" = "move container to workspace number 4";
+            "${meta}+Shift+5" = "move container to workspace number 5";
+            "${meta}+Shift+6" = "move container to workspace number 6";
+            "${meta}+Shift+7" = "move container to workspace number 7";
+            "${meta}+Shift+8" = "move container to workspace number 8";
+            "${meta}+Shift+9" = "move container to workspace number 9";
 
             # use wpctl to adjust volume
             "XF86AudioRaiseVolume" =
@@ -150,8 +152,8 @@
             "XF86MonBrightnessDown" = "exec --no-startup-id brightnessctl s 5%- && pkill -RTMIN+3 i3blocks";
 
             # change modes
-            "${mod}+r" = "mode resize";
-            "${mod}+Escape" = "mode \"${modeSystem}\"";
+            "${meta}+r" = "mode resize";
+            "${meta}+Escape" = "mode \"${modeSystem}\"";
           };
         modes = {
           resize = {
@@ -177,6 +179,10 @@
         startup = [
           {
             command = "xss-lock -n /usr/lib/xsecurelock/dimmer -l -- xsecurelock";
+            notification = false;
+          }
+          {
+            command = "xmodmap ~/.Xmodmap";
             notification = false;
           }
         ];
